43079fe52bf5ef64d20fa47ba8fb14b90a39d340
Yansu <yansu0711@gmail.com>
2013-05-15 22:34:18 +0800

    let all file copy to egg floder

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@
 import os
 from distutils.core import setup
 
-def split(path, result=None):
+def fullsplit(path, result=None):
     """
-    Split a path into components in a platform-neutral way.
+    Split a pathname into components (the opposite of os.path.join)
+    in a platform-neutral way.
     """
     if result is None:
         result = []
@@ -13,29 +14,42 @@ def split(path, result=None):
         return [tail] + result
     if head == path:
         return result
-    return split(head, [tail] + result)
+    return fullsplit(head, [tail] + result)
 
-ROOT = os.path.dirname(__file__)
-target_dirs = ['ceilometer_horizon']
+EXCLUDE_FROM_PACKAGES = []
+
+
+def is_package(package_name):
+    for pkg in EXCLUDE_FROM_PACKAGES:
+        if package_name.startswith(pkg):
+            return False
+    return True
+
 
 # Compile the list of packages available, because distutils doesn't have
 # an easy way to do this.
-packages, data_files = [], []
+packages, package_data = [], {}
+
 root_dir = os.path.dirname(__file__)
 if root_dir != '':
     os.chdir(root_dir)
+package_dir = 'package'
 
-for target_dir in target_dirs:
-    for dirpath, dirnames, filenames in os.walk(target_dir):
-        # Ignore dirnames that start with '.'
-        for i, dirname in enumerate(dirnames):
-            if dirname.startswith('.'):
-                del dirnames[i]
-        if '__init__.py' in filenames:
-            packages.append('.'.join(split(dirpath)))
-        elif filenames:
-            data_files.append([dirpath, [os.path.join(dirpath, f)
-                               for f in filenames]])
+for dirpath, dirnames, filenames in os.walk(package_dir):
+    # Ignore PEP 3147 cache dirs and those whose names start with '.'
+    dirnames[:] = [d for d in dirnames if not d.startswith('.') and d != '__pycache__']
+    parts = fullsplit(dirpath)
+    package_name = '.'.join(parts)
+    if '__init__.py' in filenames and is_package(package_name):
+        packages.append(package_name)
+    elif filenames:
+        relative_path = []
+        while '.'.join(parts) not in packages:
+            relative_path.append(parts.pop())
+        relative_path.reverse()
+        path = os.path.join(*relative_path)
+        package_files = package_data.setdefault('.'.join(parts), [])
+        package_files.extend([os.path.join(path, f) for f in filenames])
 
 setup(name="ceilometer_horizon",
       version="0.1",
@@ -43,7 +57,7 @@ setup(name="ceilometer_horizon",
       author="Brooklyn Chen",
       author_email="brooklyn.chen@canonical.com",
       packages=packages,
-      data_files=data_files,
+      package_data=package_data,
       include_package_data=True
       )
 
